be5fff885dc85590c5d61e63e25db1a65c490b89
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-05 17:56:07 -0500

    Added NavBar in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavBar from '../src/components/layout/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
