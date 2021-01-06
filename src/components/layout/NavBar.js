f4e566b459449eb6539760ea8a99c8b05b1bb142
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-06 17:28:22 -0500

    Added more bootstrap elements to <a> tag

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,7 +5,12 @@ const NavBar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a href="https://www.pokemon.com/us/pokedex/" className="navbar__text">Pokédex</a>
+        <a 
+          href="https://www.pokemon.com/us/pokedex/" 
+          className="navbar-text navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
+        >
+          Pokédex
+        </a>
       </nav>
     </div>
   )
