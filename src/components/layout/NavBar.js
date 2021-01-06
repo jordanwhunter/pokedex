eebefe608ff6de038388f8d01c57ac16f1bb0581
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-06 17:20:06 -0500

    Created navbar__text className

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a>Pokédex</a>
+        <a href="https://www.pokemon.com/us/pokedex/" className="navbar__text">Pokédex</a>
       </nav>
     </div>
   )
