ab2dad06a96f90abee4303eaba12a5ecfa4292d2
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-07 14:40:51 -0500

    Added Dashboard component within JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import NavBar from '../src/components/layout/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <>
+        <NavBar />
+        <Dashboard />
+      </>
     </div>
   );
 }
