6c8e651f5b29dea71a07bdeb1d5256300ce1308c
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-07 15:08:27 -0500

    Beginning to build state and experiemnt with hooks

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './PokemonCard';
+import axios from 'axios';
 
 const PokemonList = () => {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
   return (
-    <div className="row">
+    <div className='row'>
       <PokemonCard />
       <PokemonCard />
       <PokemonCard />
