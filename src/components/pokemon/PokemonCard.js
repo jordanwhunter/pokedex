31f18ac3e538fba9bd87aff71d0dac5bd7792e21
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-07 14:39:09 -0500

    Testing text to display on main page within a card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const PokemonCard = () => {
   return (
-    <div>
-      
+    <div className="col-md-3 col-sm-6 mb-5">
+      <div className="card">
+        <div className="card-header">
+          <h1>Testing Card Injection</h1>
+        </div>
+      </div>
     </div>
   )
 }
