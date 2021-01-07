98182ebb78bd7b28e03aa2b94f6a7c1e36b159eb
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-07 18:27:57 -0500

    Refactor

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemon }) => {
+  const [pokeName, setPokeName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [indexNum, setIndexNum] = useState('');
+
+  // const name = this.props.name;
+
   return (
     <div className='col-md-3 col-sm-6 mb-5'>
       <div className='card'>
         <div className='card-header'>
-          <h1>Testing Card Injection</h1>
+          <p>
+            {pokemon}
+          </p>
         </div>
       </div>
     </div>
   )
+  
 }
 
 export default PokemonCard
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,20 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
 import axios from 'axios';
 
 const PokemonList = () => {
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      // to avoid a race condition (in the event the user calls multiple times before request completes), we need to use a cancelToken so requests are not overridden. cancelToken is built into axios.
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      // change loading setting to false since initial loading setting is true - telling the app that the page needs to be loaded
+      setLoading(false)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+
+  // every time currentPageUrl changes, this will trigger the useEffect hook and refresh the application
+  }, [currentPageUrl])
+
+  if (loading) return "Loading Your Pokémon..."
 
   return (
-    <div className='row'>
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-    </div>
+    <>
+      {pokemon ? (
+        <div className='row'>
+          {pokemon.map(() => (
+            <PokemonCard
+              key={pokemon.name} 
+              pokemon={pokemon}
+              url={pokemon.url}
+            />
+          ))}
+        </div>
+      ) : (
+        <h1>Loading</h1>
+      )}  
+    </>
   )
 }
 
