afe9e068b762729253d4c7c32b5187b8d59bc686
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-13 17:11:42 -0500

    Created pagination page with onClick buttons that use if gates to sort between pokemon 20 at a time

---
+++ src/components/layout/Pagination.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pagination = ({ goToNextPage, goToPrevPage }) => {
+  return (
+    <div>
+      {/* this method is another way in which you can perform an IF gate in React: put the variable you want to check in front and then render everything after the ampersands. The second section will never be run if the first section results false */}
+      {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+      {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+    </div>
+  )
+}
+
+export default Pagination
