2e4ff108163b138d7b36c9001bba1be8ae23a3b1
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-14 17:35:41 -0500

    Centered previous and next buttons at bottom of page

--- src/components/layout/Pagination.js
+++ src/components/layout/Pagination.js
@@ -2,10 +2,33 @@ import React from 'react';
 
 const Pagination = ({ goToNextPage, goToPrevPage }) => {
   return (
-    <div>
+    <div className='mx-auto'>
       {/* this method is another way in which you can perform an IF gate in React: put the variable you want to check in front and then render everything after the ampersands. The second section will never be run if the first section results false */}
-      {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
-      {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+      {
+        goToPrevPage && 
+        <button 
+          onClick={goToPrevPage}
+          style={{
+            margin: 'auto'
+          }}
+        >
+          Previous
+        </button>
+      }
+      <span>
+        &nbsp;&nbsp;&nbsp;
+      </span>
+      {
+        goToNextPage && 
+        <button 
+          onClick={goToNextPage}
+          style={{
+            margin: 'auto'
+          }}
+        >
+          Next
+        </button>
+      }
     </div>
   )
 }
