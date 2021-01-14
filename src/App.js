2d8b14c9abb497911017d8afceeac1251307a719
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-14 18:10:03 -0500

    Prepping imports and rout and routes for upcoming Pokemon component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../src/components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+// import Pokemon from './components/pokemon/Pokemon';
 import backgroundImage from './pokeball.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -19,6 +20,7 @@ function App() {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Dashboard} />
+            {/* <Route exact path='/pokemon/:indexNum' component={Pokemon} /> */}
           </Switch>
         </div>
       </div>
