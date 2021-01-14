579abbd80b9701cd74cedfda6d03b8c21cba8eab
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-14 12:00:06 -0500

    Added background to App className div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../src/components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import backgroundImage from './pokeball.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <div 
+      className='App' 
+      style={
+        { background: `url(${backgroundImage})`}
+      }
+    >
       <>
         <NavBar />
         <Dashboard />
