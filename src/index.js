eb47e5420e2f6470fb8c2039957e38909187b733
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-15 15:39:36 -0500

    Imported Router and createBrowserHistory, and wrapped <App /> so entire app has access to any routing info needed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
