da624869e724252a7d848fa44f058b1488784668
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-15 16:21:10 -0500

    Pulled Pokemon object into DOM to drill necessary stats onto Pokemon component

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,10 +1,28 @@
-import React from 'react';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Pokemon = () => {
+const Pokemon = (props) => {
+  const { match } = props
+  const { params } = match
+  const { indexNum } = params
+  
+  const [pokemon, setPokemon] = useState()
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${indexNum}/`)
+      .then (res => {
+        const { data } = res
+        console.log(data)
+        setPokemon(data)
+      })
+      .catch(err => {
+        setPokemon(false)
+      })
+  }, [indexNum])
+
   return (
     <div>
-      
+      {`Test page for Pokémon #${indexNum}`}
     </div>
   )
 }
