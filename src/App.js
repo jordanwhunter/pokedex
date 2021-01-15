3036824f3dab3f75596ca0816590e86346fda194
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-15 16:20:21 -0500

    Rendered props for individual Pokemon component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from '../src/components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
-// import Pokemon from './components/pokemon/Pokemon';
+import Pokemon from './components/pokemon/Pokemon';
 import backgroundImage from './pokeball.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -20,7 +20,10 @@ function App() {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Dashboard} />
-            {/* <Route exact path='/pokemon/:indexNum' component={Pokemon} /> */}
+            <Route 
+              exact path='/pokemon/:indexNum' 
+              render={(props) => <Pokemon {...props} />} 
+            />
           </Switch>
         </div>
       </div>
