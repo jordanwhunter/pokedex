a4811eff38c774624cfb5e91fae27b9c9a2a841f
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-17 20:21:50 -0500

    Set stats to show up on page with progressbar bootstrap component

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import gif from '../../loading-wheel.gif';
 
+const type_colors = {
+  bug: 'B1C12E',
+  dark: '4F3A2D',
+  dragon: '755EDF',
+  electric: 'FCBC17',
+  fairy: 'F4B1F4',
+  fighting: '823551D',
+  fire: 'E73B0C',
+  flying: 'A3B3F7',
+  ghost: '6060B2',
+  grass: '74C236',
+  ground: 'D3B357',
+  ice: 'A3E7FD',
+  normal: 'C8C4BC',
+  poison: '934594',
+  psychic: 'ED4882',
+  rock: 'B9A156',
+  steel: 'B5B5C3',
+  water: '3295F6'
+};
+
 const Pokemon = (props) => {
   const { match } = props
   const { params } = match
@@ -15,7 +36,6 @@ const Pokemon = (props) => {
   const [catchRate, setCatchRate] = useState()
   const [eggGroups, setEggGroups] = useState()
   const [hatchSteps, setHatchSteps] = useState()
-
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${indexNum}/`)
@@ -79,150 +99,221 @@ const Pokemon = (props) => {
     const { front_default } = sprites
     const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
 
-    // stats.map(stat => {
-    //   switch (stat.stat.name) {
-    //     case 'hp':
-    //       const hp = stat['base_stat'];
-    //       console.log(`HP: ${hp}`)
-    //       break;
-    //     case 'attack':
-    //       let attack = stat['base_stat'];
-    //       console.log(`ATK: ${attack}`)
-    //       break;
-    //     case 'defense':
-    //       let defense = stat['base_stat'];
-    //       console.log(`DEF: ${defense}`)
-    //       break;
-    //     case 'speed':
-    //       let speed = stat['base_stat'];
-    //       console.log(`SPEED: ${speed}`)
-    //       break;
-    //     case 'special-attack':
-    //       let specialAttack = stat['base_stat'];
-    //       console.log(`SP. ATK: ${specialAttack}`)
-    //       break;
-    //     case 'special-defense':
-    //       let specialDefense = stat['base_stat'];
-    //       console.log(`SP. DEF: ${specialDefense}`)
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // })
+    let [
+      hp, 
+      attack, 
+      defense, 
+      speed, 
+      specialAttack, 
+      specialDefense
+    ] = ''
+
+    stats.map(stat => {
+      switch (stat.stat.name) {
+        case 'hp':
+          hp = stat['base_stat'];
+          console.log(`HP: ${hp}`)
+          break;
+        case 'attack':
+          attack = stat['base_stat'];
+          console.log(`ATK: ${attack}`)
+          break;
+        case 'defense':
+          defense = stat['base_stat'];
+          console.log(`DEF: ${defense}`)
+          break;
+        case 'speed':
+          speed = stat['base_stat'];
+          console.log(`SPEED: ${speed}`)
+          break;
+        case 'special-attack':
+          specialAttack = stat['base_stat'];
+          console.log(`SP. ATK: ${specialAttack}`)
+          break;
+        case 'special-defense':
+          specialDefense = stat['base_stat'];
+          console.log(`SP. DEF: ${specialDefense}`)
+          break;
+        default:
+          break;
+      }
+    })
 
     return (
       <>
-        <div>
-          { `${id}. ` }
-          { name
-            .toLowerCase()
-            .split(' ')
-            .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-            .join(' ')
-          }
-          <img
-            src={ front_default }
-            alt='Pokémon Sprite' 
-          />
-          <br />
-          {/* { `${height} Decimeters` } */}
-          { '~ ' + 
-            (Math.round((height * 0.328084 + 0.0001) * 100) / 100) + ' ft / ' + 
-            (Math.round((height * 10.006 + 0.0001) * 100) / 100) + ' cm' 
-          }
-          <br />
-          <br />
-          {/* { `${weight} Hectograms` } */}
-          { '~ ' + 
-            (Math.round((weight * 0.220462 + 0.0001) * 100) / 100) + ' lbs / ' +
-            (Math.round((weight * 0.10001 + 0.0001) * 100) / 100) + ' kg' 
-          }
-          <br />
-          <br />
-          { types.map(typeInfo => {
-            const { type } = typeInfo;
-            const { name } = type;
-            return <div 
-              key={name}
-            >
-              {`${name
-                  .toLowerCase()
-                  .split(' ')
-                  .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-                  .join(' ')
-                }`
-              }
+        <div className='col'>
+          <div className='card'>
+            <div className='card-header'>
+              <div className='row'>
+                <div className='col-5'>
+                  <h5>{`Pokémon Index #${indexNum}`}</h5>
+                </div>
+                <div className='col-7'>
+                  <div className='float-right'>
+                    { types.map(typeInfo => {
+                      const { type } = typeInfo;
+                      const { name } = type;
+                      return <span 
+                        key={name}
+                        className='badge badge-primary badge-pill mr-1'
+                        style={{
+                          backgroundColor: `#${type_colors[name]}`,
+                          color: 'white'
+                        }}
+                      >
+                        {`${name
+                            .toLowerCase()
+                            .split(' ')
+                            .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                            .join(' ')
+                          }`
+                        }
+                      </span>
+                    }) }
+                  </div>
+                </div>
+              </div>
             </div>
-          }) }
-          <br />
-          <br />
-          { abilities.map(abilityInfo => {
-            const { ability } = abilityInfo;
-            const { name } = ability;
-            return `${name
-              .toLowerCase()
-              .split('-')
-              .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-              .join('-')
-            }`
-          }).join(', ') }
-          <br />
-          <br />
-          { stats.filter(stat => {
-            if (stat.effort > 0) {
-              return true;
-            }
-            return false;
-          }) 
-          .map(statInfo => {
-            const { effort, stat } = statInfo
-            const { name } = stat
-
-            return `${effort} ${name
-              .toLowerCase()
-              .split(' ')
-              .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-              .join(' ')
-            }`
-          }).join(', ') }
-          <br />
-          <br />
-          { bio }
-          <br />
-          <br />
-          { `Catch Rate: ${catchRate}%` }
-          <br />
-          <br />
-          { eggGroups.map(group => {
-            const { name } = group
-            return `${name
-              .toLowerCase()
-              .split(' ')
-              .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-              .join(' ')
-            }`
-          }).join(', ') }
-          <br />
-          <br />
-          { `Hatch Steps: ${hatchSteps}`}
-          <br />
-          <br />
-          { stats.map(statInfo => {
-            const { base_stat, stat } = statInfo
-            const { name } = stat
-            return `${name
-              .toLowerCase()
-              .split(' ')
-              .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-              .join(' ')
-            }: ${base_stat}`
-          }).join(', ') }
-          <br />
-          <br />
-          <img
-            src={ fullImageUrl }
-            alt='Pokémon Large Pic' 
-          />
+            <div className='card-body'>
+              <div className='row align-items-center'>
+                <div className='col-md-3'>
+                  <img
+                    src={ fullImageUrl }
+                    alt='Pokémon Large Pic' 
+                    className='card-img-top rounded mx-auto mt-2'
+                  />  
+                </div>
+                <div className='col-md-9'>
+                  <h5 className='mx-auto'>
+                    { name
+                      .toLowerCase()
+                      .split(' ')
+                      .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                      .join(' ')
+                    }
+                    <img
+                      src={ front_default }
+                      alt='Pokémon Sprite' 
+                    />
+                  </h5>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>HP</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${hp}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{hp}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>Attack</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${attack}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{attack}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>Sp. Attack</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${specialAttack}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{specialAttack}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>Defense</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${defense}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{defense}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>Sp. Defense</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${specialDefense}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{specialDefense}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row align-items-center'>
+                    <div className='col-12 col-md-3'>Speed</div>
+                    <div className='col-12 col-md-9'>
+                      <div className='progress'>
+                        <div 
+                          className='progress-bar'
+                          role='progressbar'
+                          style={{
+                            width: `${speed}%`
+                          }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        >
+                          <small>{speed}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     )
@@ -243,3 +334,78 @@ const Pokemon = (props) => {
 }
 
 export default Pokemon
+
+// { `${id}. ` }
+
+// <br />
+// {/* { `${height} Decimeters` } */}
+// { '~ ' + 
+//   (Math.round((height * 0.328084 + 0.0001) * 100) / 100) + ' ft / ' + 
+//   (Math.round((height * 10.006 + 0.0001) * 100) / 100) + ' cm' 
+// }
+// <br />
+// <br />
+// {/* { `${weight} Hectograms` } */}
+// { '~ ' + 
+//   (Math.round((weight * 0.220462 + 0.0001) * 100) / 100) + ' lbs / ' +
+//   (Math.round((weight * 0.10001 + 0.0001) * 100) / 100) + ' kg' 
+// }
+// <br />
+// <br />
+
+// <br />
+// <br />
+// { abilities.map(abilityInfo => {
+//   const { ability } = abilityInfo;
+//   const { name } = ability;
+//   return `${name
+//     .toLowerCase()
+//     .split('-')
+//     .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+//     .join('-')
+//   }`
+// }).join(', ') }
+// <br />
+// <br />
+// { stats.filter(stat => {
+//   if (stat.effort > 0) {
+//     return true;
+//   }
+//   return false;
+// }) 
+// .map(statInfo => {
+//   const { effort, stat } = statInfo
+//   const { name } = stat
+
+//   return `${effort} ${name
+//     .toLowerCase()
+//     .split(' ')
+//     .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+//     .join(' ')
+//   }`
+// }).join(', ') }
+// <br />
+// <br />
+// { bio }
+// <br />
+// <br />
+// { `Catch Rate: ${catchRate}%` }
+// <br />
+// <br />
+// { eggGroups.map(group => {
+//   const { name } = group
+//   return `${name
+//     .toLowerCase()
+//     .split(' ')
+//     .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+//     .join(' ')
+//   }`
+// }).join(', ') }
+// <br />
+// <br />
+// { `Hatch Steps: ${hatchSteps}`}
+// <br />
+// <br />
+
+// <br />
+// <br />
