a2e465493b135ffe515da986fd2b54e6546e9e77
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-01-19 15:38:09 -0500

    Refactor

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import styled from 'styled-components';
 
 const NavBar = () => {
+
   return (
     <div>
-      <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
+      <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top flex-container'>
         <a 
           href='/' 
           className='navbar-text navbar-brand col-sm-3 col-md-2 mr-0 align-items-center'
